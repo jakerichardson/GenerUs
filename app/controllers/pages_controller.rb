b061e864eb51bf93901a48e597bf0b93ec783232
Jake Richardson <jakerichardson5@gmail.com>
2014-03-12 15:28:34 -0700

    Ready for Heroku

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def methodology
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 GenerUs::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "methodology" => "pages#methodology"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
